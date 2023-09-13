e4332584145bb195709458fd484d3bc7cf3c72d9
Sami <s.babouche@esi-sba.dz>
2023-09-13 11:45:41 +0100

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.getElementById("create-task-form")
+  const tasksList = document.getElementById("tasks")
+
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form)
+    const taskDescription = formData.get("new-task-description").trim();
+ 
+
+    if (taskDescription !== "") {
+      const newTaskItem = document.createElement("li")
+      
+
+
+
+      
+
+
+
+
+      const deleteButton = document.createElement("button")
+      deleteButton.textContent = "🗑"
+      deleteButton.classList.add("delete-button");
+
+    
+
+      deleteButton.addEventListener("click", () => {
+        tasksList.removeChild(newTaskItem)
+      });
+      newTaskItem.innerHTML = `<span id="desc">- Description: ${taskDescription}  `
+
+      //
+    
+
+
+
+
+      newTaskItem.appendChild(deleteButton)
+      
+      tasksList.appendChild(newTaskItem)
+
+      form.reset()
+    }
+  });
+
+  });
